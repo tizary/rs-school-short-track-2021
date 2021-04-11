2a77085ff8231ff216a034e05da8b1f15ad7abe5
tizary <tizary@mail.ru>
2021-04-12 00:18:17 +0300

    15

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,10 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const result = email.split('@');
+  const last = result.length - 1;
+  return result[last];
 }
 
 module.exports = getEmailDomain;
